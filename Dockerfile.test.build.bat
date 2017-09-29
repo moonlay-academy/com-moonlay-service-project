2f48b42c4051984d11fa38b51e9605930cae7efe
Tris Setiawan <tris.setiawan@yahoo.com>
2017-09-29 21:03:25 +0700

    update docker travis

--- Dockerfile.test.build.bat
+++ Dockerfile.test.build.bat
@@ -1,7 +1,7 @@
 rmdir "bin" /s /q
-docker build -f Dockerfile.test.build -t com-moonlay-service-project-webapi:test-build .
+docker build --no-cache -f Dockerfile.test.build -t com-moonlay-service-project-webapi:test-build .
 docker rm com-moonlay-service-project-webapi-test-build-container
 docker create --name com-moonlay-service-project-webapi-test-build-container com-moonlay-service-project-webapi:test-build
 md bin
 docker cp com-moonlay-service-project-webapi-test-build-container:/out ./bin/publish
-docker build -f Dockerfile.test -t com-moonlay-service-project-webapi:test .
+docker build --no-cache -f Dockerfile.test -t com-moonlay-service-project-webapi:test .
